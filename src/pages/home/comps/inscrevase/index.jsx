be390e2bd11a08b4705d61c91e0d35911bc5cf49
Miguel <Miguel@gmail.com>
2024-08-30 18:25:09 -0300

    component inscrevase

---
+++ src/pages/home/comps/inscrevase/index.jsx
@@ -0,0 +1,47 @@
+import "./index.scss"
+import { useEffect, useState } from "react"
+
+export default function Parceiros() {
+    return (
+
+        <div className="inscrevase">
+
+            <div className="parte1">
+
+                <img className="ondinhas" src="https://cdn.discordapp.com/attachments/1276616678330990602/1279152060151103538/8b4372c1ed1ce594eb936336b26fbdb8.png?ex=66d366b0&is=66d21530&hm=f2f1126c7fbf949ab69d0e97670b2d3407f98fe9e2ae503f0e859f6c952ab49b&" alt="" />
+
+            </div>
+            <div className="partedireita">
+
+                <div className="text">
+                    <h1 className="p1">Inscreva-se no nosso site <br />
+                        ou acessando o nosso bot <br />
+                        no WhatsApp
+
+
+                    </h1>
+                    <div className="botoes">
+
+                        <button className="botao1">Inscreva-se</button>
+                        <button className="botao2">Whatsapp</button>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+    )
+
+
+}
+
