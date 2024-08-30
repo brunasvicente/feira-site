91b0d48de99774309aa298229bfa975f3ea92c93
Mariana <mazzinoob@gmail.com>
2024-08-30 14:47:43 -0300

    alterando1

---
+++ src/pages/home/comps/parceiros/index.jsx
@@ -0,0 +1,28 @@
+import './index.scss'
+import { Link } from 'react-router-dom'
+
+export default function Parceiros(){
+    return(
+
+        <div className="secao parceiros ">
+            <div className="titulo"><h1>Parceiros</h1></div>
+
+            <div className="parceiros">
+
+            <div className="parceiro 1">
+                <img src="/assets/images/empfeminino.png" alt="empfeminino" />
+                <img src="/assets/images/danimatos2.png" alt="danimatos" />
+                <img src="/assets/images/cuja3.png" alt="cuja" />
+                <img src="/assets/images/opcenter4.png" alt="opencenter" />
+            </div>
+            <div className="parceiro 2">
+                <img src="/assets/images/italo5.png" alt="italo" />
+                <img src="/assets/images/uni6.png" alt="unisa" />
+                <img src="/assets/images/stb7.png" alt="stb" />
+
+
+            </div>
+            </div>
+        </div>
+    )
+}
