4bfe37d746317a1c801fb8dfafc473dd27e92193
fefemariaa <vieiradealmeidam003@gmail.com>
2024-08-29 22:12:09 -0300

    .

---
+++ src/pages/chegada/pages/page1/index.jsx
@@ -0,0 +1,17 @@
+import './index.scss'
+import { Link } from 'react-router-dom'
+
+
+
+
+export default function Chegada(){
+    return(
+        <div className="secao1">
+
+
+
+        </div>
+
+        
+    )
+}
